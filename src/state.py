1cfa3b7a6f4693bed73742d5b35bd45010312b0a
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-12-08 00:04:50 +0900

    update: add state to bytes function

--- src/state.py
+++ src/state.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from struct import pack, unpack
 
 # Shared with Master
 
@@ -32,3 +33,96 @@ class ArmState:
 class CollectionState:
     speed: float = 0.0  # -1 ~ 1
     angle: float = 0.0  # 0 ~ 45
+
+
+def convert_to_list(
+    system_state: SystemState,
+    footer_state: FooterState,
+    arm_state: ArmState,
+    collection_state: CollectionState,
+) -> list[float]:
+    return [
+        # SystemState
+        system_state.is_running,
+        # FooterState
+        footer_state.left_speed,
+        footer_state.right_speed,
+        footer_state.left_front_flipper,
+        footer_state.left_back_flipper,
+        footer_state.right_front_flipper,
+        footer_state.right_back_flipper,
+        # ArmState
+        arm_state.base_angle,
+        arm_state.mid_angle,
+        arm_state.tip_angle,
+        arm_state.rotate,
+        arm_state.gripper_speed,
+        # CollectionState
+        collection_state.speed,
+        collection_state.angle,
+    ]
+
+
+def convert_from_list(
+    data: list,
+) -> tuple[
+    SystemState,
+    FooterState,
+    ArmState,
+    CollectionState,
+]:
+    return (
+        SystemState(
+            is_running=data[0],
+        ),
+        FooterState(
+            left_speed=data[1],
+            right_speed=data[2],
+            left_front_flipper=data[3],
+            left_back_flipper=data[4],
+            right_front_flipper=data[5],
+            right_back_flipper=data[6],
+        ),
+        ArmState(
+            base_angle=data[7],
+            mid_angle=data[8],
+            tip_angle=data[9],
+            rotate=data[10],
+            gripper_speed=data[11],
+        ),
+        CollectionState(
+            speed=data[12],
+            angle=data[13],
+        ),
+    )
+
+
+def pack_state(
+    SystemState,
+    FooterState,
+    ArmState,
+    CollectionState,
+) -> bytes:
+    return pack(
+        "?fffffffffffff",
+        *convert_to_list(
+            SystemState,
+            FooterState,
+            ArmState,
+            CollectionState,
+        )
+    )
+
+
+def unpack_state(data: bytes) -> tuple[
+    SystemState,
+    FooterState,
+    ArmState,
+    CollectionState,
+]:
+    return convert_from_list(
+        unpack(
+            "?fffffffffffff",
+            data,
+        )
+    )
